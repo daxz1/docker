0e22c2f31bf51f1de5fbf35097b608454d94e217
DSC <davinder.cheema@ymail.com>
2020-07-07 11:01:40 +0100

    [CHORE] Added ESLinting for fun

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const PORT = 3000;
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const PORT = 3000
 
-const instantMongoCrud = require('express-mongo-crud');
+const instantMongoCrud = require('express-mongo-crud')
 
-mongoose.connect('database:27017/mongocrud');
+mongoose.connect('database:27017/mongocrud')
 
 const options = {
-    host: `localhost:${PORT}`
+  host: `localhost:${PORT}`
 }
 
-app.use(bodyParser.json());
-app.use(instantMongoCrud(options));
+app.use(bodyParser.json())
+app.use(instantMongoCrud(options))
 
-app.get('/',  (req,res)=> {
-    res.send('Hello World');
-});
+app.get('/', (req, res) => {
+  res.send('/')
+})
 
-app.listen(PORT, ()=> {
-    console.log('Started');
+app.listen(PORT, () => {
+  console.log('Started')
 })
